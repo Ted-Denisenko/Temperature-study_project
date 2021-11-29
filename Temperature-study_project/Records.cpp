4a19a94efd45a7cc4085b1ea688847c0ab10f168
Ted-Denisenko <91020107+Ted-Denisenko@users.noreply.github.com>
2021-11-29 21:14:49 +0300

    Класс SaveToFile удален, т.к. состоял из одного метода сохранения, метод перемещен в класс Records. Сохранение в файл теперь работает исправно. Начальные значения списка теперь создаются случайно(работает неправильно - все значения одинаковые) Конструктор и декструктор перенесены в public область класса Добавлены методы вывода записанных температур, наибольшего и наименьшего значения температуры

--- Temperature-study_project/Records.cpp
+++ Temperature-study_project/Records.cpp
@@ -1,6 +1,29 @@
 #include "Records.h"
 #include <iostream>
 #include <fstream>
+#include <cstdlib> // дл€ функций rand() и srand()
+
+double Records::getRandomNumber(double min, double max)
+{
+	srand(static_cast<unsigned int>(time(0)));
+	static const double fraction = 1.0 / (static_cast<double>(RAND_MAX) + 1.0);
+	// –авномерно распредел€ем рандомное число в нашем диапазоне
+	return static_cast<double>(rand() * fraction * (max - min + 1) + min);
+}
+
+Records::Records()
+{
+	//заполн€ем список случайными числами
+	for (int count = 0; count < 10; ++count)
+	{
+		temperatures.push_back(getRandomNumber(-15.0, 30.0));
+	}
+}
+
+Records::~Records()
+{
+
+}
 
 void Records::new_record(float temperature)
 {
@@ -34,7 +57,7 @@ void Records::remove_record()
 
 void Records::save()
 {
-	std::ofstream out("records.txt");          // создаем поток дл€ записи и св€зываем его с файлом
+	std::ofstream out("records.txt"); // создаем поток дл€ записи и св€зываем его с файлом
 	if (out.is_open())
 	{
 		auto temp = getTemperature();
@@ -43,4 +66,20 @@ void Records::save()
 			out << *iter << std::endl;
 		}
 	}
+}
+
+void Records::showTemperature()
+{	
+	std::cout << "Recorded temperatures are: " << std::endl;
+	auto temp = getTemperature();
+	for (auto iter = temp.begin(); iter != temp.end(); iter++)
+	{
+		std::cout << *iter << std::endl;
+	}
+}
+
+void Records::showExtremums()
+{
+	std::cout << "Highest temperature is " << Records::getHighest() << " degrees at Celsius" << std::endl;
+	std::cout << "Lowest temperature is " << Records::getLowest() << " degrees at Celsius" << std::endl;
 }
--- Temperature-study_project/Records.h
+++ Temperature-study_project/Records.h
@@ -8,16 +8,22 @@
 
 class Records
 {
-	std::list<double> temperatures{0.1, 0.16, 0.14 };
+	std::list<double> temperatures{};
 	int counter{ 0 };
 	float lowest{ 0 };// самая низкая температура
 	float highest{ 0 };// высокая
+
 public:
+	Records();
+	~Records();
 	std::list<double> getTemperature();
 	void new_record(float temperature);
 	void remove_record();
-	void showHighest() { std::cout << highest << " is the highest temperature recorded" << std::endl; }
-	void showLowest() { std::cout << lowest << " is the lowest temperature recorded" << std::endl; }
+	void showTemperature();
+	void showExtremums();
+	double getHighest() { return highest; }
+	double getLowest() { return lowest; }
+	double getRandomNumber(double min, double max);
 	void save();
 
 
